e417369fdbb9221f4a580dd35c98a7e9251335c1
bridgetbidigare <90158212+bridgetbidigare@users.noreply.github.com>
2021-11-17 17:31:50 -0600

    Organized JS

--- src/javascript.js
+++ src/javascript.js
@@ -1,3 +1,101 @@
+// 📅 Date & Time
+
+let now = new Date();
+
+function formatDate() {
+  let currentDate = document.querySelector("#date");
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[now.getDay()];
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+  let month = months[now.getMonth()];
+
+  let date = now.getDate();
+  let year = now.getFullYear();
+  currentDate.innerHTML = `${day}, ${month} ${date}, ${year}`;
+  return `${day}, ${month} ${date}, ${year}`;
+}
+
+function formatTime() {
+  let currentTime = document.querySelector("#time");
+  let hour = now.getHours();
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`
+  }
+  hours = ((hour + 11) % 12 + 1);
+  if (hour > 11) {
+    currentTime.innerHTML = `${hours}:${minutes}PM`
+  } else {
+    currentTime.innerHTML = `${hour}:${minutes}AM`
+  }
+}
+
+
+// 🔎 Search
+
+function searchCity(event) {
+  event.preventDefault();
+  let cities = document.querySelector("#city-search");
+  let h2 = document.querySelector("h2");
+  let searchUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cities.value}&appid=${apiKey}&units=${unit}`;
+  let degreec = document.querySelector("#degree-symbol");
+
+  h2.innerHTML = cities.value
+  axios.get(searchUrl).then(showTemp);
+
+  if (degreec.innerHTML = "°C") {
+    degreec.innerHTML = "°F";
+    celsius.innerHTML = "°C";
+    document.getElementById('celsius').onclick = convertTempC;
+  }
+}
+
+let cityInput = document.querySelector("#search-bar");
+cityInput.addEventListener("submit", searchCity);
+
+
+// 📍 Get Current Location
+
+function showPosition(position) {
+  let temp = document.querySelector("#degree-temp");
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let geoLocUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${unit}`;
+  axios.get(geoLocUrl).then(showTemp);
+}
+
+function getPosition(position) {
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let locationButton = document.querySelector("#location-button");
+locationButton.addEventListener("click", getPosition);
+
+
+// 🌡 Show Temperature
+
 let apiKey = "75c66def1228b9939b902b974cffcda2";
 let unit = "imperial";
 let city = "Chicago";
@@ -100,92 +198,8 @@ function showTemp(response) {
 
 axios.get(apiUrl).then(showTemp);
 
-let now = new Date();
 
-function formatDate() {
-  let currentDate = document.querySelector("#date");
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-  let day = days[now.getDay()];
-
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-  ];
-  let month = months[now.getMonth()];
-
-  let date = now.getDate();
-  let year = now.getFullYear();
-  currentDate.innerHTML = `${day}, ${month} ${date}, ${year}`;
-  return `${day}, ${month} ${date}, ${year}`;
-}
-
-function formatTime() {
-  let currentTime = document.querySelector("#time");
-  let hour = now.getHours();
-  let minutes = now.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`
-  }
-  hours = ((hour + 11) % 12 + 1);
-  if (hour > 11) {
-    currentTime.innerHTML = `${hours}:${minutes}PM`
-  } else {
-    currentTime.innerHTML = `${hour}:${minutes}AM`
-  }
-}
-
-function searchCity(event) {
-  event.preventDefault();
-  let cities = document.querySelector("#city-search");
-  let h2 = document.querySelector("h2");
-  let searchUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cities.value}&appid=${apiKey}&units=${unit}`;
-  let degreec = document.querySelector("#degree-symbol");
-
-  h2.innerHTML = cities.value
-  axios.get(searchUrl).then(showTemp);
-
-  if (degreec.innerHTML = "°C") {
-    degreec.innerHTML = "°F";
-    celsius.innerHTML = "°C";
-    document.getElementById('celsius').onclick = convertTempC;
-  }
-}
-
-let cityInput = document.querySelector("#search-bar");
-cityInput.addEventListener("submit", searchCity);
-
-function showPosition(position) {
-  let temp = document.querySelector("#degree-temp");
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-  let geoLocUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${unit}`;
-  axios.get(geoLocUrl).then(showTemp);
-}
-
-function getPosition(position) {
-  navigator.geolocation.getCurrentPosition(showPosition);
-}
-
-let locationButton = document.querySelector("#location-button");
-locationButton.addEventListener("click", getPosition);
+// 🧮 Convert Temp to °C & °F
 
 function convertTempC(event) {
   event.preventDefault();
